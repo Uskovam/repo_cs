6a18d08e7e52e6d4996efc8a5589a216b5b9d810
Uskova <uscovam@gmail.com>
2022-10-02 20:10:52 +0300

    привет,

---
+++ task05/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+        Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+        Console.Write("привет, ");
+        Console.WriteLine(username);
+}
